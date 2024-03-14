3529afb4e7858772abdcaffdd7b574afbfd61178
Poldus <leopold.canou@gmail.com>
2024-03-14 01:02:02 +0100

    added SlideButton

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 export default {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {}, // Enable CSS nesting before Tailwind CSS
     tailwindcss: {},
     autoprefixer: {},
   },
